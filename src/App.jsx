f545e2cfa08a4f0481242f723613d78c4a50db14
L-David-26 <davidlez2mgk@gmail.com>
2021-10-21 08:47:23 -0500

    notes reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-const 
-
-
-
+import { useReducer } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NoteReducer from './Reducers/NoteReducer';
 
 function App() {
+  const [state, dispatch] = useReducer(NoteReducer, []);
 
+  const addNote = (evt) => {
+    evt.preventDefault();
+    const { target } = evt;
+    const content = target.note.value;
+  };
   return (
-   <div>
-        
-   </div>
-  )
+    <div>
+      <form onSumbit={addNote}>
+        <input type="text" name="note" />
+        <button>add note</button>
+      </form>
+      <ul>
+        {state.map((i) => {
+          return <li key={i.id}>{i.note}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
